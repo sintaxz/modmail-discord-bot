b562aa164ce1336d2799f136472aba6f754dace5
hope <91838759+sintaxz@users.noreply.github.com>
2022-06-14 18:40:20 +0300

    Update index.js

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ client.login(process.env.TOKEN);
 const express = require('express')
 const app = express();
 const port = 3000
-app.get('/', (req, res) => res.send('Odd is better.'))
+app.get('/', (req, res) => res.send('KylixDevelopers'))
 app.listen(port, () =>
 console.log(`Your app is listening a http://localhost:${port}`)
 );
