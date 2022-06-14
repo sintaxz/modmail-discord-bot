c50c951a95b885b414e760a96c1a69a8476b77c6
hope <91838759+sintaxz@users.noreply.github.com>
2022-06-14 18:31:58 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,45 @@
+const Discord = require('discord.js');
+const allIntents = new Discord.Intents(32767);
+const client = new Discord.Client({
+  messageCacheLifetime: 60,
+  fetchAllMembers: false,
+  messageCacheMaxSize: 10,
+  restTimeOffset: 0,
+  restWsBridgetimeout: 100,
+  allowedMentions: {
+    parse: ["roles", "users", "eveoryone"],
+    repliedUser: true,
+  },
+  partials: ["MESSAGE", "CHANNEL", "REACTION"],
+  intents: allIntents,
+});
+
+client.login(process.env.TOKEN);
+const express = require('express')
+const app = express();
+const port = 3000
+app.get('/', (req, res) => res.send('Odd is better.'))
+app.listen(port, () =>
+console.log(`Your app is listening a http://localhost:${port}`)
+);
+const charModMail = require('char-mod-mail');
+client.on("ready", () => {
+    console.log(`Logged in as ${client.user.tag}!`);
+
+  //Important Changing Area
+charModMail.ModMail(client, {
+  guildID: "xxxxx", //put your guild id here
+  categoryID: "xxxxxxxxx", //put your category id here
+  staffRole: "xxxxxxxx", //put your staff role id here
+  embedColor: "#2f3136", //change the hax color code if you want
+  anonymousReply: false/true, //make it false if only the staff can reply the user or make it true so anyone can reply.
+  closedTitle: "Your Mod Mail Has Been Closed",
+  closedMessage: "A Staff Member Has Deleted You Mod Mail!",
+  staffOpenedTitle: "User Opened Mod Mail",
+  staffOpenedMessage: "The User Opened A Mod Mail And Is Now Wait For A Reply!",
+  userOpenedTitle: "Mod Mail Created",
+  userOpenedMessage: "You Created A Mod Mail Ticket!",
+  wrongEmoji: "❎", // if you want you can change but don't change it recommaned.
+  rightEmoji: "✅" // if you want you can change but don't change it recommaned.
+})
+});
